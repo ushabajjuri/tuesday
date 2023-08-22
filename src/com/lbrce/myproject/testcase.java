382bc4d665a2a02791ceaf35c86f3a0dbab458f3
Usha.Bajjuri <Usha.Bajjuri@LAPTOP-12E6KTGC>
2023-08-22 10:48:28 +0530

    first commit

---
+++ src/com/lbrce/myproject/testcase.java
@@ -0,0 +1,10 @@
+package com.lbrce.myproject;
+
+public class testcase {
+	public static void main(String[] args)
+	{
+		int a=10,b=20,c;
+	    c=a+b;
+	    System.out.println("value of c:"+c);
+	}
+}
